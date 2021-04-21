286569b3ded810a5848d100763adbc1024bdb6cf
Eric101201 <z077452958>
2021-04-21 22:53:12 +0800

    030

--- bot.py
+++ bot.py
@@ -13,35 +13,10 @@ async def on_ready():
     with open('file.json', mode='r', encoding='UTF8') as jfile:
         jdate1 = json.load(jfile)
     
-    ggg = jdate1["1"]
+    ggg = re.sub(r"(\d)$", "\\1級", jdate1["1"]).replace("-", "弱").replace("+", "強")
     sec = jdate1["2"]
     
-    if ggg == "0":
-        await message_channel.send(f"地震速報，預估震度{ggg}級，{sec}秒後抵達")
-    elif ggg == "1":
-        await message_channel.send(f"地震速報，預估震度{ggg}級，{sec}秒後抵達")
-    elif ggg == "2":
-        await message_channel.send(f"地震速報，預估震度{ggg}級，{sec}秒後抵達")
-    elif ggg == "3":
-        await message_channel.send(f"地震速報，預估震度{ggg}級，{sec}秒後抵達")
-    elif ggg == "4":
-        await message_channel.send(f"地震速報，預估震度{ggg}級，{sec}秒後抵達")
-    
-    elif ggg == "5-":
-        await message_channel.send(f"地震速報，預估震度5弱，{sec}秒後抵達")
-        
-    elif ggg == "5+":
-        await message_channel.send(f"地震速報，預估震度5強，{sec}秒後抵達")
-        
-    elif ggg == "6-":
-        await message_channel.send(f"地震速報，預估震度6弱，{sec}秒後抵達")
-        
-    elif ggg == "6+":
-        await message_channel.send(f"地震速報，預估震度6強，{sec}秒後抵達")
-        
-    elif ggg == "7":
-        await message_channel.send(f"地震速報，預估震度{ggg}級，{sec}秒後抵達")
-
+    await message_channel.send(f"地震速報，預估震度{ggg}，{sec}秒後抵達")
     await asyncio.sleep(5)
     await bot.close()
 
